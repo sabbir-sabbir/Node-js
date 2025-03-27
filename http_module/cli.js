b45c00627e3b2e6280df0f9b5965efcaaa3306be
sabbir-sabbir <iosabbir7890@gmail.com>
2025-03-27 21:17:38 +0600

    add

--- http_module/cli.js
+++ http_module/cli.js
@@ -4,3 +4,16 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
+
+const todos = [];
+
+const showMenu=()=> {
+    console.log("\n Add a task");
+    console.log("2: View Task");
+    console.log("3: Exit");
+    
+    
+    
+}
+
+showMenu()
