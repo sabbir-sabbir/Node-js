3d980010d33587ea7db387d7d6a0b531b6f0a54b
sabbir-sabbir <iosabbir7890@gmail.com>
2025-03-27 22:03:18 +0600

    add

--- http_module/cli.js
+++ http_module/cli.js
@@ -8,11 +8,37 @@ const rl = readline.createInterface({
 const todos = [];
 
 const showMenu=()=> {
-    console.log("\n Add a task");
-    console.log("2: View Task");
-    console.log("3: Exit");
+    console.log("\n 1: Add a task");
+    console.log(" 2: View Task");
+    console.log(" 3: Exit");
+    rl.question("Choose an option:  ", handleInput )
     
     
+    
+}
+
+const handleInput = (option)=> {
+    if(option === "1"){
+        rl.question("Enter the task: - - "   , (task)=> {
+            todos.push(task);
+            console.log("Task added successfully ------ (*_*) (*_*) (*_*) (*_*) (*_*) (*_*) (*_*) (*_*) (*_*) (*_*) (*_*) (*_*)");
+            showMenu()
+        })
+    } else if (option === "2"){
+        console.log("\n Your Todo list");
+        todos.forEach((task, index)=> {
+          console.log(`${index + 1}. ${task}`);
+        })
+        showMenu()
+    } else if(option === "3"){
+        console.log("GOOD BYE");
+        
+        rl.close()
+    } else {
+        console.log("Invalid  Input Please try again");
+        showMenu()
+        
+    }
     
 }
 
