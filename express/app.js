8d6b804f2903b8e9419ecd5999a70047be45c8ed
sabbir-sabbir <iosabbir7890@gmail.com>
2025-04-03 20:16:49 +0600

    initialize express application and add body-parser middleware

--- express/app.js
+++ express/app.js
@@ -1,3 +1,14 @@
 import express from 'express';
 
 const app = express();
+
+app.get('/', (req, res)=> {
+    res.send("hello world!")
+});
+
+
+const PORT = 8000;
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+    
+})
