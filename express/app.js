8a837836a9635f1e1c67cb3c68c2ce2635f74313
sabbir-sabbir <iosabbir7890@gmail.com>
2025-04-16 22:56:22 +0600

    Fix import statement for PORT and add schema validation for port number

--- express/app.js
+++ express/app.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { PORT } from './env';
+import { PORT } from './env.js';
 
 const app = express();
 
@@ -11,7 +11,7 @@ app.get('/about', (req, res)=> {
 });
 
 
-// const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
     
--- express/env.js
+++ express/env.js
@@ -1,2 +1,38 @@
-import {z} from 'zod';
-const ageSchema = z.number().min(18).max(100).int();
+import {z, ZodError} from 'zod';
+
+const portSchema = z.coerce.number().min(1).max(3000).default(3000);
+
+export const PORT = portSchema.parse(process.env.PORT);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const ageSchema = z.number().min(18).max(100).int();
+// const age = 19;
+
+// try {
+//     const validAge = ageSchema.parse(age); 
+// console.log(validAge); // 18
+// } catch (error) {
+//     if(error instanceof ZodError){
+//         console.log(error.issues[0].message);
+        
+//     } else {
+//         console.log("An unexpected error occurred:", error);
+//     }
+// }
