e5d66e610a13e42249d3f102d163acb4c209f0e6
sabbir-sabbir <iosabbir7890@gmail.com>
2025-04-17 20:25:54 +0600

    Refactor profile route to remove unused home page handler and streamline response

--- express/app.js
+++ express/app.js
@@ -7,18 +7,8 @@ const app = express();
 const staticPath = path.join(import.meta.dirname, "public");
 app.use( express.static(staticPath));
 
-app.get("/", (req, res)=> {
-    
-    const forHomePage = path.join(import.meta.dirname, "public", "index.html");
+app.get("/profile/:username", (req, res)=> {
    
-    res.sendFile(forHomePage, (err)=> {
-        if(err){
-            console.log(err);
-            res.status(500).send("Internal Server Error");
-        } else {
-            console.log("File sent successfully");
-        }
-    });
 })
 
 
