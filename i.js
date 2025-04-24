7e5c285168df0647fee8772b641d375df51a82de
sabbir-sabbir <iosabbir7890@gmail.com>
2025-04-25 00:24:33 +0600

    Refactor i.js to improve code organization and readability

--- i.js
+++ i.js
@@ -1,2 +1,10 @@
+const {add, substract, multiplication, division, PI} = require("./math");
+console.log(add(5,6));
+
+
+
+
+
+// this are for only commonjs
 console.log(__dirname);
 console.log(__filename);
