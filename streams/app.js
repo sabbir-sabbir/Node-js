b1b3019b7a5d2b63dba780494fc2fcfcadac0608
sabbir-sabbir <iosabbir7890@gmail.com>
2025-04-24 21:33:29 +0600

    Implement data chunk handling and stream end management for file streaming

--- streams/app.js
+++ streams/app.js
@@ -6,7 +6,29 @@ const outputFilePath = path.join(import.meta.dirname, "output.txt");
 
 const readStream = createReadStream(inputFilePath, {encoding: 'utf-8', highWaterMark: 16});
 const writeStream = createWriteStream(outputFilePath);
-readStream.pipe(writeStream);
+
+
+// Listen for data chunks
+readStream.on("data", (chunk)=> {
+    console.log("Buffer (chunk):", Buffer.from(chunk));
+    console.log(("Received chunk: ", chunk));
+    writeStream.write(chunk);
+    
+    
+});
+
+// Handle stream end
+readStream.on("end", ()=> {
+    console.log("Our Work is done here!");
+    console.log("Closing the write stream...");
+    console.log("-----------------------------------------------------------------------");
+    writeStream.end(); // close the write stream
+});
+
+
+
+
+// readStream.pipe(writeStream);
 
 // handle eror
 readStream.on("error", (err)=> console.error("Error:", err));
